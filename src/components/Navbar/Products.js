489d09622ff4f622b53707ae8cf116a2f19ee8ec
GirishEgo <Jai.girishego@gmail.com>
2025-03-13 13:56:20 +0530

    some things no neede

---
+++ src/components/Navbar/Products.js
@@ -0,0 +1,28 @@
+const Products = [
+  {
+    id: 1,
+    name: "Product 1",
+    subProducts: [
+      { id: "1-1", title: "Sub Product 1A" },
+      { id: "1-2", title: "Sub Product 1B" },
+    ],
+  },
+  {
+    id: 2,
+    name: "Product 2",
+    subProducts: [
+      { id: "2-1", title: "Sub Product 2A" },
+      { id: "2-2", title: "Sub Product 2B" },
+    ],
+  },
+  {
+    id: 3,
+    name: "Product 3",
+    subProducts: [
+      { id: "3-1", title: "Sub Product 3A" },
+      { id: "3-2", title: "Sub Product 3B" },
+    ],
+  },
+];
+
+export default Products;
