82cbbeabae60ba663009a4db4cba696e43d60de8
GirishEgo <Jai.girishego@gmail.com>
2025-03-19 15:35:25 +0530

    app context lazy load pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,18 @@
-import { Routes, Route } from "react-router-dom";
-import { lazy, Suspense } from "react";
 import Navbar from "./components/Navbar/Navbar";
 import Footer from "./components/Footer";
+import { LoaderProvider } from "./context/LoaderContext";
 import Loader from "./components/Loader";
-import ProductDetails from "./components/products/ProductDetails";
-
-// Lazy load components
-const Home = lazy(() => import("./pages/Home"));
-const About = lazy(() => import("./pages/About"));
-const Contact = lazy(() => import("./pages/Contact"));
-const Products = lazy(() => import("./pages/Products"));
-const Resources = lazy(() => import("./pages/Resources"));
-
-const AppContent = () => {
-  return (
-    <Suspense fallback={<Loader />}>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/products" element={<Products />} />
-        <Route path="/resources" element={<Resources />} />
-        {/* Dynamic Product Route */}
-        <Route path="/products/:id" element={<ProductDetails/>} />
-      </Routes>
-    </Suspense>
-  );
-};
+import AppContent from "./AppContent";
 
 const App = () => {
   return (
-    <>
+    <LoaderProvider>
+      {/* ✅ Global Loader */}
+      <Loader />
       <Navbar />
       <AppContent />
       <Footer />
-    </>
+    </LoaderProvider>
   );
 };
 
