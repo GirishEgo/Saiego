184ec8cddcf91c88d82fb758f630de4f4f026500
GirishEgo <Jai.girishego@gmail.com>
2025-04-09 21:41:17 +0530

    Bg Balls removed

--- src/App.jsx
+++ src/App.jsx
@@ -2,25 +2,24 @@ import Navbar from "./components/Navbar/Navbar";
 import Footer from "./components/Footer";
 import { LoaderProvider } from "./context/LoaderContext";
 import { CallProvider } from "./context/CallContext";
-
 import Loader from "./components/Loader";
 import AppContent from "./AppContent";
 
 const App = () => {
   return (
     <LoaderProvider>
-      <div class="floating-balls">
-        <div class="ball ball1"></div>
-        <div class="ball ball2"></div>
-        <div class="ball ball3"></div>
-      </div>
+      {/* <div className="floating-balls">
+        <div className="ball ball1"></div>
+        <div className="ball ball2"></div>
+        <div className="ball ball3"></div>
+      </div> */}
       {/* ✅ Global Loader */}
       <CallProvider>
         <Loader />
         <Navbar />
         <AppContent />
+        <Footer />
       </CallProvider>
-      {/* <Footer /> */}
     </LoaderProvider>
   );
 };
