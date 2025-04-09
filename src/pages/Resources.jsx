028dcfc7932cda68a6ad5c50e41e9b44989fea6f
GirishEgo <Jai.girishego@gmail.com>
2025-04-09 21:51:03 +0530

    Resources Outlet

--- src/pages/Resources.jsx
+++ src/pages/Resources.jsx
@@ -1,5 +1,17 @@
+import React from "react";
+import { Outlet } from "react-router-dom";
+
 const Resources = () => {
-  return <h1>Resources</h1>;
+  return (
+    <div className="resources-page">
+      {/* <h1>Resources</h1> */}
+      {/* Optional: Navigation links inside Resources */}
+      {/* <Link to="heat-calculator">Heat Calculator</Link> */}
+
+      {/* Where nested content will render */}
+      <Outlet />
+    </div>
+  );
 };
 
 export default Resources;
