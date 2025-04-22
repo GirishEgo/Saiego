e62235bed89d2684422efab8cd1bc922761c04ea
GirishEgo <Jai.girishego@gmail.com>
2025-04-22 18:47:39 +0530

    helmet Provider Intergrated

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,19 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from "react-router-dom";
-
+import { HelmetProvider } from "react-helmet-async";
 import './index.css'
 import App from './App.jsx'
-import { LoaderProvider } from './context/LoaderContext.jsx';
+// import { LoaderProvider } from './context/LoaderContext.jsx';
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     {/* <LoaderProvider> */}
-      <BrowserRouter>
+    <BrowserRouter>
+      <HelmetProvider>
         <App />
-      </BrowserRouter>
+      </HelmetProvider>
+    </BrowserRouter>
     {/* </LoaderProvider> */}
   </StrictMode>
 );
