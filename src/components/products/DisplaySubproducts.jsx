009209bed2df0bb7a645226b34e114df925ce01f
GirishEgo <Jai.girishego@gmail.com>
2025-04-23 15:36:46 +0530

    index upadtes

--- src/components/products/DisplaySubproducts.jsx
+++ src/components/products/DisplaySubproducts.jsx
@@ -137,9 +137,9 @@ const DisplaySubproducts = () => {
                   initial="hidden"
                   animate="visible"
                 >
-                  {item.subProducts.map((subP) => (
+                  {item.subProducts.map((subP, subix) => (
                     <ProductCard
-                      key={subP.id}
+                      key={subix}
                       to={`/Products/${finalLayout.id}/${subP.id}`}
                       img={subP.resolvedImg}
                       title={subP.title}
