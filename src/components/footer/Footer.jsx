c39681c18f61c8c8c3a9046fffdc4200b73863ca
GirishEgo <Jai.girishego@gmail.com>
2025-05-05 20:20:46 +0530

    Logo image added

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -73,7 +73,7 @@ const Footer = () => {
   return (
     <footer className="footer">
       <motion.div className="footer-logo" {...fadeUp(0.1)}>
-        <h6>Saiego</h6>
+        <img style={{width:"10rem"}} src="Logo.png" alt="" />
       </motion.div>
 
       <motion.p className="footer-tagline" {...fadeUp(0.2)}>
