e859d9bad532b94a86b2b4fe85ff07d1fa472064
GirishEgo <Jai.girishego@gmail.com>
2025-05-12 14:55:03 +0530

    product cards logo

--- src/components/products/ProductCard.jsx
+++ src/components/products/ProductCard.jsx
@@ -15,26 +15,31 @@ const itemVariants = {
 
 const ProductCard = ({ to, img, title, description }) => (
   <motion.div
-    className="product-card"
-    variants={itemVariants}
-    initial="hidden"
-    animate="visible"
-  >
-    <Link to={to} className="product-link">
-      {img && <LazyImage src={img} alt={title} className="product-image" />}
-      <h4>{title}</h4>
-      <p>
-        {description ? (
-          <>
-            {description.split(" ").slice(0, 20).join(" ")}...
-            <span className="read-more"> Read more</span>
-          </>
-        ) : (
-          <span className="click-here-button">Click here</span>
-        )}
-      </p>
-    </Link>
-  </motion.div>
+  className="product-card"
+  variants={itemVariants}
+  initial="hidden"
+  animate="visible"
+>
+  <div className="product-logo">
+    <img src="/Logo2.png" alt={title} width={"100rem"}/>
+  </div>
+  
+  <Link to={to} className="product-link">
+    {img && <LazyImage src={img} alt={title} className="product-image" />}
+    <h4>{title}</h4>
+    <p>
+      {description ? (
+        <>
+          {description.split(" ").slice(0, 20).join(" ")}...
+          <span className="read-more"> Read more</span>
+        </>
+      ) : (
+        <span className="click-here-button">Click here</span>
+      )}
+    </p>
+  </Link>
+</motion.div>
+
 );
 
 
