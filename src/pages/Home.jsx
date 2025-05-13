4fccac8affd3e14edcca0eeb843f0847a6ec24d6
GirishEgo <Jai.girishego@gmail.com>
2025-05-13 11:07:32 +0530

    marquee added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,12 +5,14 @@ import Carousel from "../components/Hero/Carousel";
 import HomepaeCard from "../components/Homepage/HomepaeCard";
 import Certifiat from "../components/certificats/Certifiat";
 import HomeAboutSection from "../components/Homepage/HomeAboutSection";
+import Marquee from "../components/Marquee/Marquee";
 
 const Home = () => {
 
   return (
     <div>
       <Carousel />
+      <Marquee/>
       {/* <h1>{data.title}</h1>
       <p>{data.body}</p> */}
       <HomepaeCard />
